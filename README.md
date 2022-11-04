64d14ac6e40c09551fe51722e0c8fac5c4c94620
mmsstudent2021 <mmsstudent2021@gmail.com>
2022-11-04 15:55:28 +0630

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# Introduction
+ကျောင်းသားတွေအတွက်စမ်းပြတာပါခင်ဗျာ။
+
+# Instillation
+* `npm install hhz-show-my-name`
+* `improt showMyName form 'hhz-show-my-name'`
+* `console.log(showMyName('any name'))`
---
+++ index.js
@@ -0,0 +1,12 @@
+export const myName = "hein htet zan";
+export const showMyName = (newName = myName) => console.log(newName);
+export const showMyNameUI = (newName = myName, top = 0, right = 0) => {
+  const div = document.createElement("div");
+  div.style.color = "white";
+  div.style.backgroundColor = "black";
+  div.style.position = "fixed";
+  div.style.top = top;
+  div.style.right = right;
+  div.style.fontSize = 100 + "px";
+  div.innerHTML = `<h1>${newName}</h1>`;
+};
